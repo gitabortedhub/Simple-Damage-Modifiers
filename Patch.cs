98f47e65c93f909721e1f4e10788fe65b7bf67aa
gitabortedhub <gitabortedhub>
2025-02-24 22:56:55 -0800

    First

---
+++ Patch.cs
@@ -0,0 +1,65 @@
+﻿using SPT.Reflection.Patching;
+using System.Collections.Generic;
+using System.Reflection;
+using EFT.HealthSystem;
+using EFT;
+using EFT.InventoryLogic;
+
+namespace Simple_Damage_Modifiers
+{
+    public class InitColliderPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(BodyPartCollider).GetMethod("InitColliderSettings", BindingFlags.Public | BindingFlags.Instance);
+        }
+        [PatchPostfix]
+        private static void PatchPostfix(ref BodyPartCollider __instance)
+        {
+            if (!Plugin.FixArms.Value) return;
+            if (__instance.BodyPartType == EBodyPart.LeftArm || __instance.BodyPartType == EBodyPart.RightArm)
+            {
+                __instance.penetrationDamageMod = 1f;
+                __instance.PenetrationChance = 1f;
+                __instance.PenetrationLevel = 0f;
+            }
+        }
+    }
+    public class ApplyArmorDamagePatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(ArmorComponent).GetMethod("ApplyDurabilityDamage", BindingFlags.Instance | BindingFlags.Public);
+        }
+
+        [PatchPrefix]
+        private static void Prefix(ref ArmorComponent __instance, ref float armorDamage, List<ArmorComponent> armorComponents)
+        {
+            armorDamage *= Plugin.DamageModifierDurability.Value;
+        }
+    }
+    public class ApplyDamagePatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(ActiveHealthController).GetMethod("ApplyDamage", BindingFlags.Public | BindingFlags.Instance);
+        }
+        [PatchPrefix]
+        private static void PatchPrefix(ref ActiveHealthController __instance, EBodyPart bodyPart, ref float damage, DamageInfoStruct damageInfo)
+        {
+            damage *= Plugin.DamageModifierGeneral.Value;
+            if (damageInfo.DamageType == EDamageType.Bullet)
+            {
+                damage *= Plugin.DamageModifierBallistic.Value;
+                if (bodyPart == EBodyPart.Head)
+                {
+                    damage *= Plugin.DamageModifierHead.Value;
+                }
+            }
+            if (damageInfo.DamageType == EDamageType.Explosion)
+            {
+                damage *= Plugin.DamageModifierExplosive.Value;
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,52 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+
+namespace Simple_Damage_Modifiers
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger;
+        internal static ConfigEntry<bool> FixArms { get; set; }
+        internal static ConfigEntry<float> DamageModifierGeneral { get; set; }
+        internal static ConfigEntry<float> DamageModifierBallistic { get; set; }
+        internal static ConfigEntry<float> DamageModifierHead { get; set; }
+        internal static ConfigEntry<float> DamageModifierExplosive { get; set; }
+        internal static ConfigEntry<float> DamageModifierDurability { get; set; }
+
+        private void Awake()
+        {
+            FixArms = Config.Bind<bool>("General",
+                                         "Fix Arms Decreasing Damage",
+                                         true,
+                                         "Prevents arms from protecting the thorax from bullet damage");
+            DamageModifierGeneral = Config.Bind<float>("General",
+                                         "Global Damage Modifier",
+                                         1f,
+                                         "All damage sources");
+            DamageModifierBallistic = Config.Bind<float>("General",
+                                         "Ballistic Damage Modifier",
+                                         1f,
+                                         "Bullet damage sources");
+            DamageModifierHead = Config.Bind<float>("General",
+                                         "Ballistic Damage Modifier (Head)",
+                                         1f,
+                                         "Extra ballistic damage modifier applied to head");
+            DamageModifierExplosive = Config.Bind<float>("General",
+                                         "Explosive Damage Modifier",
+                                         1f,
+                                         "Explosive damage sources");
+            DamageModifierDurability = Config.Bind<float>("General",
+                                         "Armor Durability Damage Modifier",
+                                         1f,
+                                         "Damage received by armor");
+            // Plugin startup logic
+            Logger = base.Logger;
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+            new InitColliderPatch().Enable();
+            new ApplyDamagePatch().Enable();
+            new ApplyArmorDamagePatch().Enable();
+        }
+    }
+}
